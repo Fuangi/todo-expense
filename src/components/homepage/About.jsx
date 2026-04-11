c622710383ac18192365a28357f5325766ed319c
Vanessa Fuangi <vanessafuangi@gmail.com>
2026-04-11 17:34:03 -0400

    Started the about section and have left it on pause

---
+++ src/components/homepage/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>Who are we?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        eligendi unde, exercitationem distinctio quaerat mollitia amet dolore
+        hic reprehenderit deserunt rem labore quasi error quos?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        eligendi unde, exercitationem distinctio quaerat mollitia amet dolore
+        hic reprehenderit deserunt rem labore quasi error quos?
+      </p>
+
+      <div></div>
+      <h2>Why did we?</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, et
+        quae ut temporibus beatae nam iusto quos, unde dolorem quam nemo qui,
+        expedita illo dignissimos. Ullam minima libero possimus quidem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, et
+        quae ut temporibus beatae nam iusto quos, unde dolorem quam nemo qui,
+        expedita illo dignissimos. Ullam minima libero possimus quidem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, et
+        quae ut temporibus beatae nam iusto quos, unde dolorem quam nemo qui,
+        expedita illo dignissimos. Ullam minima libero possimus quidem!
+      </p>
+    </div>
+  );
+}
+
+export default About;
--- src/features/Homepage.jsx
+++ src/features/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 import Home from '../components/homepage/Home';
+import About from '../components/homepage/About';
 
 function Homepage () {
   return (
     <div className='homepage'>
       <Header />
       <Home />
+      <About />
 
       <Footer />
     </div>
