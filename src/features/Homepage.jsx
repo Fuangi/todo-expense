6caac5666756c9134efe359ceafdade5357f0514
Vanessa Fuangi <vanessafuangi@gmail.com>
2026-04-12 14:03:52 -0400

    Setup the files for the services section

--- src/features/Homepage.jsx
+++ src/features/Homepage.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import Header from '../layout/Header';
-import Footer from '../layout/Footer';
-import Home from '../components/homepage/Home';
-import About from '../components/homepage/About';
+import React from "react";
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+import Home from "../components/homepage/Home";
+import About from "../components/homepage/About";
+import Services from "../components/homepage/Services";
 
-function Homepage () {
+function Homepage() {
   return (
-    <div className='homepage'>
+    <div className="homepage">
       <Header />
       <Home />
+      <Services />
       <About />
 
       <Footer />
     </div>
   );
-};
+}
 
 export default Homepage;
