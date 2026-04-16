85e244dcbe46f8d62ee875a26216126aef6be287
Vanessa Fuangi <vanessafuangi@gmail.com>
2026-04-16 12:17:24 -0400

    Did the mobile responsiveness logic

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
 
 function Header() {
   const location = useLocation();
+  const navRef = useRef();
+
+  const toggleMenu = () => {
+    navRef.current.classList.toggle("responsiveNav");
+  };
 
   return (
     <header className="homepage-header homepage-layout">
-      <section className="logo">V'sTE</section>
+      <div className="logo">V'sTE</div>
 
-      <nav className="header-nav">
+      <nav className="header-nav" ref={navRef}>
         <NavHashLink smooth to="/" end>
           Home
         </NavHashLink>
@@ -24,7 +32,15 @@ function Header() {
         <NavHashLink smooth to="auth">
           Join Us
         </NavHashLink>
+
+        <button className="close-nav" onClick={toggleMenu}>
+          <FaTimes />
+        </button>
       </nav>
+
+      <button className="mobile-nav" onClick={toggleMenu}>
+        <FaBars />
+      </button>
     </header>
   );
 }
