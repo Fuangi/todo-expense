ec0b726d2b5ff54eed46f61f5857989424e31738
Vanessa Fuangi <vanessafuangi@gmail.com>
2026-04-19 13:21:29 -0400

    Controlled the login form usig state

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    const loginDetails = [{ loginEmail, loginPassword }];
+    console.log(loginDetails);
+
+    setLoginEmail("");
+    setLoginPassword("");
+  }
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleLogin}>
       <h2>Login</h2>
       <p>Please enter your email and password to log into you account</p>
       <div>
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={loginEmail}
+          onChange={(e) => setLoginEmail(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={loginPassword}
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
       </div>
       <button className="form-btn">Login</button>
     </form>
