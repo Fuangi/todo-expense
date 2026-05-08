cdf1db680a82a6bede90d55ae688c55e27ce97a1
Vanessa Fuangi <vanessafuangi@gmail.com>
2026-05-08 12:42:26 -0400

    finish: the task summary top section on the dashboard

---
+++ src/assets/dashboardData.js
@@ -0,0 +1,6 @@
+export const taskSummaryData = [
+  { summaryName: "Total Tasks", qty: 20, numChanged: 4, changeState: 1 },
+  { summaryName: "Completed Tasks", qty: 20, numChanged: 4, changeState: 0 },
+  { summaryName: "Ongoing Tasks", qty: 20, numChanged: 4, changeState: 1 },
+  { summaryName: "Pending Tasks", qty: 20, numChanged: 4, changeState: 1 },
+];
--- src/features/Dashboard.jsx
+++ src/features/Dashboard.jsx
@@ -1,19 +1,54 @@
 import React from "react";
 import DashSideNav from "../components/dashboard/DashSideNav";
 import DashLayout from "../layout/DashLayout";
+import { BsArrowUpRightCircle } from "react-icons/bs";
+import { RxCaretUp } from "react-icons/rx";
+import { taskSummaryData } from "../assets/dashboardData";
+import DashTaskSummaryCard from "../components/dashboard/DashTaskSummaryCard";
 
 function Dashboard() {
   return (
     <DashLayout>
       <section className="dashboard">
-        <div className="left-container">
-          <div className="todo-summary">Todo List summary</div>
-          <div className="expense-summary">Expense summary</div>
+        <div className="dash-intro">
+          <div>
+            <h1>My Dashboard</h1>
+            <p>
+              Plan, prioritize, accomplish and track everything that matters.
+            </p>
+          </div>
+          <div className="intro-btns">
+            <button className="dashboard-main">Add Task</button>
+            <button>Add Expense</button>
+          </div>
         </div>
-        <div className="right-container">
+        <div className="task-summary">
+          {taskSummaryData.map((summary, i) => (
+            <DashTaskSummaryCard
+              className={`${i === 0 && "dashboard-main"}`}
+              key={i}
+              summaryName={summary.summaryName}
+              qty={summary.qty}
+              numChanged={summary.numChanged}
+              changeState={summary.changeState}
+            />
+          ))}
+        </div>
+        <div className="task-analysis">
+          <div className="task-analysis-graph">
+            <h3>Task analysis</h3>
+          </div>
+          <div className="reminders">
+            <h3>Reminders</h3>
+          </div>
+          <div className="tasks-mini-view">
+            <h3>Tasks</h3>
+          </div>
+        </div>
+        <div className="expense-chat-summary">
           <div className="chats"> Your chats</div>
-          <div>Your Collaborators</div>
         </div>
+        <div className="expense-graph">Your Collaborators</div>
       </section>
     </DashLayout>
   );
